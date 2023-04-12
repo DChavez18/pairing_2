1297528a2504137f0c96b9acea71693b792dc4d2
Derek Chavez <frrenose17@yahoo.com>
2023-04-12 16:18:08 -0600

    Add insert method

--- friends.rb
+++ friends.rb
@@ -3,3 +3,9 @@ arrays = ["amy", "joshua", "morgan", "dominic", "samantha"]
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+
+
+destinations = ["seattle", "denver", "new zealand", "tibet", "nepal"]
+
+destinations.insert(2, "nigeria")
+p destinations
